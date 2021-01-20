90cf515872d8cd507017e1d6fa6b60418f6d4636
Eugênio Carvalho <eugenio9008@gmail.com>
2021-01-19 22:03:05 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const testExpression = document.querySelector('#test-expression');
+const res = document.querySelector('#res');
+const expression = prompt('Expressão regular:');
+const re = new RegExp(expression);
+testExpression.focus();
+testExpression.addEventListener('input', () => {
+
+  // console.log(re)
+
+  if (!isNaN(testExpression.value)) {
+    if (re.test(Number(testExpression.value))) {
+      res.setAttribute('style', 'background-color: rgb(91, 245, 130);');
+
+      res.innerHTML = 'Palavra aceita';
+    } else {
+      res.setAttribute('style', 'background-color: rgb(245, 91, 91);');
+
+      res.innerHTML = 'Palavra rejeitada';
+    }
+  } else {
+    if (re.test(testExpression.value)) {
+      res.setAttribute('style', 'background-color: rgb(91, 245, 130);');
+
+      res.innerHTML = 'Palavra aceita';
+    } else {
+      res.setAttribute('style', '  background-color: rgb(245, 91, 91);');
+
+      res.innerHTML = 'Palavra rejeitada';
+    }
+
+  }
+});
